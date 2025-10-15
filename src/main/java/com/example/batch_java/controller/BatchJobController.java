5e96afa796d5f41740917e1bb95b6f1fd40d9836
Pradit Yamsook <praditcry@gmail.com>
2025-10-15 18:23:27 +0700

    [FEAT]: rest controller job

---
+++ src/main/java/com/example/batch_java/controller/BatchJobController.java
@@ -0,0 +1,50 @@
+package com.example.batch_java.controller;
+
+import jdk.jpackage.internal.ApplicationLayout;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/batch")
+public class BatchJobController {
+    private final JobLauncher jobLauncher;
+    private final ApplicationContext context;
+
+    @PostMapping("/run/{jobName}")
+    public ResponseEntity<String> runJob(@PathVariable String jobName) {
+        try {
+            Job job = (Job) context.getAttribute(jobName);
+
+            JobParameters params = new JobParametersBuilder()
+                    .addLong("run.id", System.currentTimeMillis())
+                    .toJobParameters();
+            jobLauncher.run(job, params);
+
+            return ResponseEntity.ok("Job '" + jobName + "' started successfully");
+        } catch (JobInstanceAlreadyCompleteException e) {
+            throw new RuntimeException(e);
+        } catch (JobExecutionAlreadyRunningException e) {
+            throw new RuntimeException(e);
+        } catch (JobParametersInvalidException e) {
+            throw new RuntimeException(e);
+        } catch (JobRestartException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Failed: " + e.getMessage());
+        }
+    }
+}
